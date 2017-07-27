8839db769f539d71af682e4fc3cd18751e6548b6
Nahedson <nahedson@gmail.com>
2017-07-27 12:59:03 -0300

    Mostrar dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,11 +22,45 @@ export class AppComponent implements OnInit {
     ngOnInit(): void {
         this.ufs = this.ufService.getAll();
         this.dados_da_samu = this.samuService.getAllMunicipiosAtendidosPorEstado();
+        this.defineTitle();
+        this.defineArea();
+        this.defineMedia();
     }
+    meu_estado_id = 32;
+    title = undefined;
     defineTitle() : void {
       for(let uf of this.ufs){
         if(uf.id === 32) this.title = uf.nome;
       }
     }
-    title = "app";
+    meu_estado_area = undefined;
+    defineArea() : void {
+      for(let uf of this.ufs){
+        if(uf.id === 32) this.meu_estado_area = uf.area;
+      }
+    }
+    meu_estado_media = 0;
+    i_media = 0;
+    soma_municipios = 0;
+    defineMedia() : void {
+      for(let VALORES of this.dados_da_samu){
+        if(VALORES.uf_id === 32) {
+          this.soma_municipios += VALORES.valor;
+          this.i_media++;
+        }
+      }
+      this.meu_estado_media = this.soma_municipios/this.i_media;
+    }
+
+    dados = [];
+    defineDados() : void {
+      for(let VALORES of this.dados_da_samu){
+        if(VALORES.uf_id === 32) {
+         this.dados.push(VALORES);
+        }
+      }
+      // this.dados = this.dados_da_samu
+      //   .filter((item) => item.uf_id == 32);
+    }
+
 }
